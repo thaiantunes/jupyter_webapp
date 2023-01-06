77fac7117175962c024111327aa87ebdc0e245b7
Thainara Antunes <thainaraantunes@gmail.com>
2023-01-05 23:09:00 -0300

    correcting guests quantity widget

--- food_calc.py
+++ food_calc.py
@@ -1,25 +1,25 @@
 from math import ceil
 
 def food_calc(guests, veggie_guests, red, chicken, corn):
-    num_tomato = ceil((guests-veggie_guests) + 1.5*veggie_guests)
-    num_zucchini = ceil((guests-veggie_guests) + 1.2*veggie_guests)
-    num_onions = ceil(((guests-veggie_guests) + 1.5*veggie_guests)*0.8)
+    num_tomato = ceil(guests + 1.5*veggie_guests)
+    num_zucchini = ceil(guests + 1.2*veggie_guests)
+    num_onions = ceil((guests + 1.5*veggie_guests)*0.8)
     if corn=='Yes':
-        num_corn = ceil((guests-veggie_guests) + 1.5*veggie_guests)
+        num_corn = ceil(guests + 1.5*veggie_guests)
     else:
         num_corn = None
     if red=='Yes' and chicken=='No':
-        num_red = (guests-veggie_guests) * 500
+        num_red = guests * 500
         num_chicken = None
     elif red=='No' and chicken=='No':
         num_red = None
         num_chicken = None
     elif red=='No' and chicken=='Yes':
         num_red = None
-        num_chicken = (guests-veggie_guests) * 600
+        num_chicken = guests * 600
     else:
-        num_red = (guests-veggie_guests) * 250
-        num_chicken = (guests-veggie_guests) * 300
+        num_red = guests * 250
+        num_chicken = guests * 300
     return num_tomato, num_zucchini, num_onions, num_corn, num_red, num_chicken
 
 
